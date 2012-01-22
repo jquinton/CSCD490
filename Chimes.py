aaea33a531cae3a36b1708cdf1275a99a61c3ba2
eric <eric@eric-linux-PC.(none)>
2012-01-22 14:52:47 -0800

    Eric's additions to Chimes.py

--- Chimes.py
+++ Chimes.py
@@ -6,13 +6,15 @@ import Tkinter as tk
 pygame.init()
 
 color=[]
-black = [ 0, 0, 0]
-white = [255,255,255]
-height = 800 
-width = 600
+BLACK = [ 0, 0, 0]
+WHITE = [255,255,255]
+HEIGHT = 800 
+WIDTH = 600
+NOTE_CIRCLE_RADIUS = 18
+NOTE_STARTING_Y = 500
  
 # Set the height and width of the screen
-size=[height,width]
+size=[HEIGHT,WIDTH]
 screen=pygame.display.set_mode(size)
 pygame.display.set_caption("Listen2ABC")
  
@@ -21,21 +23,27 @@ circle_list=[]
 line_list_start=[]
 line_list_stop=[]
 flag_list_stop=[]
+
+
  
 # Loop n times and add a note shape in each position. Also generate random color for each note.
 for i in range(100, 750, 50): #(start, stop, increment)
     x=i
-    y=500
+    y = NOTE_STARTING_Y
     circle_list.append([x,y])
-    line_list_start.append([x+13, y])
-    line_list_stop.append([x+13, y-40])
+    line_list_start.append([x+15, y])
+    line_list_stop.append([x+15, y-40])
     flag_list_stop.append([x+25, y-20])
-    r=random.randrange(0,255)
-    g=random.randrange(0,255)
-    b=random.randrange(0,255)
+    r=random.randrange(100,255)
+    g=random.randrange(100,255)
+    b=random.randrange(100,255)
     color.append([r, g, b])
  
 clock = pygame.time.Clock()
+
+#temporarly have processingNeeded set here. This should be set true when the user clicks on listen
+processingNeeded = True
+
  
 #Loop until the user clicks the close button.
 done=False
@@ -45,26 +53,92 @@ while done==False:
             done=True 
  
     # Make the screen background temporarily black until image processing is figured out
-    screen.fill(black)
+    screen.fill(BLACK)
  
-    # Process each shape in the list
-    for i in range(len(circle_list)):
-            # Draw the shape (music note?)
-            pygame.draw.circle(screen,color[i],circle_list[i],15)
-            pygame.draw.line(screen,color[i],line_list_start[i], line_list_stop[i],  3)
-            pygame.draw.line(screen,color[i],line_list_stop[i], flag_list_stop[i],  3)
-           
-            # Move the shape up one pixel if 100 pixels from the top has not been reached
-            # circle_list[i][x] where x=1 means move vertical or x=0 means move horizontal
-            xd, yd = circle_list[i]
-            if yd > 100:
-                circle_list[i][1]-=1
-                line_list_start[i][1]-=1
-                line_list_stop[i][1]-=1
-                flag_list_stop[i][1]-=1
+    #check to see if the animating loop needs to be entered. This should be set true when the
+    #user clicks on listen
+    if processingNeeded:
+        # Process each shape in the list
+        
+        tempLetter = "A" #temporary simulation of different note heights
+        for i in range(len(circle_list)):
+            
+            #temporary simulation of different note heights 
+            tempheight = i * 24 + 100
+            tempAsciiVal = ord(tempLetter)
+            tempAsciiVal += 1
+            tempLetter = chr(tempAsciiVal)
+            
+            #draw a note assending for height pixels from the top of the screen
+            #stop is the height of the screen - the starting position of the notes - tempheight + 1 to allow letter 
+            #animation when the note reaches the top.
+            for k in range(0, HEIGHT - (HEIGHT - NOTE_STARTING_Y) - tempheight + 1, 1): #(start, stop, increment)
+                # clear the last shape
+                clearRect = pygame.Rect(((circle_list[i][0] - (NOTE_CIRCLE_RADIUS)),  line_list_stop[i][1]),  (NOTE_CIRCLE_RADIUS * 3, NOTE_CIRCLE_RADIUS * 4))
+                pygame.draw.rect(screen, BLACK, clearRect )
+                # Draw the shape (music note?)
+                pygame.draw.circle(screen,color[i],circle_list[i],NOTE_CIRCLE_RADIUS)
+                pygame.draw.line(screen,color[i],line_list_start[i], line_list_stop[i],  (NOTE_CIRCLE_RADIUS / 4))
+                pygame.draw.line(screen,color[i],line_list_stop[i], flag_list_stop[i],  (NOTE_CIRCLE_RADIUS / 4))
+                          
+                # Move the shape up one pixel if 'tempheight' pixels from the top has not been reached
+                # circle_list[i][x] where x=1 means move vertical or x=0 means move horizontal
+                xd, yd = circle_list[i]
+                if yd > tempheight:
+                    circle_list[i][1]-=1
+                    line_list_start[i][1]-=1
+                    line_list_stop[i][1]-=1
+                    flag_list_stop[i][1]-=1
+                    
+                    # draw the letter
+                    if pygame.font:
+                        font = pygame.font.Font(None, 22)
+                        text = font.render(tempLetter, 1, BLACK)
+                        textpos = pygame.Rect((circle_list[i][0] - (NOTE_CIRCLE_RADIUS / 4), circle_list[i][1] - (NOTE_CIRCLE_RADIUS / 4)), (10, 10))
+                        screen.blit(text, textpos)
+                 
+                # The shape has reached its destination. animate the letter
+                else:
+                    # draw the letter increasing size 
+                    for j in range(24, 32, 1):#(start, stop, increment)
+                        if pygame.font:
+                            #clear the old letter
+                            pygame.draw.circle(screen,color[i],circle_list[i],NOTE_CIRCLE_RADIUS)
+                            
+                            #draw the new letter
+                            font = pygame.font.Font(None, j)
+                            text = font.render(tempLetter, 2, BLACK)
+                            textpos = pygame.Rect((circle_list[i][0] - (NOTE_CIRCLE_RADIUS / 4)-(j-22), circle_list[i][1] - (NOTE_CIRCLE_RADIUS / 4)-(j-22)), (10, 10))
+                            screen.blit(text, textpos)
+                            
+                            clock.tick(40)
+                            
+                        # Update the screen with the drawing
+                        pygame.display.flip()
+                        
+                    # draw the letter decreasing size 
+                    for j in range(30, 22, -1):#(start, stop, increment)
+                        if pygame.font:
+                            #clear the old letter
+                            pygame.draw.circle(screen,color[i],circle_list[i],NOTE_CIRCLE_RADIUS)
+                            
+                            #draw the new letter
+                            font = pygame.font.Font(None, j)
+                            text = font.render(tempLetter,  2, BLACK)
+                            textpos = pygame.Rect((circle_list[i][0] - (NOTE_CIRCLE_RADIUS / 4)-(j-22), circle_list[i][1] - (NOTE_CIRCLE_RADIUS / 4)-(j-22)), (10, 10))
+                            screen.blit(text, textpos)
+                            
+                            clock.tick(40)
+                            
+                        # Update the screen with the drawing
+                        pygame.display.flip()
          
-    # Update the screen with the drawing
-    pygame.display.flip()
-    clock.tick(40)
-             
+                
+                # Update the screen with the drawing
+                pygame.display.flip()
+                clock.tick(200)
+        
+        # done animating. no need to continue looping        
+        processingNeeded = False
+        
 pygame.quit ()
