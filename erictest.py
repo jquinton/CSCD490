54bd67c9972cc7b6c804c44c80fd7e1c82459e1d
eric <eric@eric-linux-PC.(none)>
2012-02-06 20:49:13 -0800

    added images to erictest.py

--- erictest.py
+++ erictest.py
@@ -1,6 +1,9 @@
 import os, pygame
 from pygame.locals import *
 
+SCREENWIDTH = 1200
+SCREENHEIGHT = 900
+
 #the note object that will assend the screen
 class ImageObject:
     def __init__(self, image, Xcord,  Ycord, speed,  letter):
@@ -14,7 +17,7 @@ class ImageObject:
 
 #function to load an image
 def load_image(name):
-    path = os.path.join('res', name)
+    path = os.path.join('Images/Letters/', name)
     return pygame.image.load(path).convert()
     
     
@@ -31,7 +34,7 @@ def redrawTiles(screen,  background,  bottomTiles):
 def addTile(screen,  bottomTiles,  image,  letters):
     #load the image
     letterTileImage = load_image(image)
-    letterTile = ImageObject(letterTileImage,  (len(bottomTiles)*50+10), 550,   1,  letters[len(letters)-1])
+    letterTile = ImageObject(letterTileImage,  (len(bottomTiles)*40+10), SCREENHEIGHT - 40,   1,  letters[len(letters)-1])
     #draw the image
     screen.blit(letterTile.image, letterTile.pos)
     #add to array
@@ -50,9 +53,9 @@ def play_sound(sound_path):
     pygame.mixer.music.play()
 
 #first to run when program starts
-def main():
+def main():    
     pygame.init()
-    screen = pygame.display.set_mode((1200, 600))
+    screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
     
     # fill sound array with sound files
     sound_list=[]
@@ -77,8 +80,7 @@ def main():
     #used so animation displays at a reasonable speed
     clock = pygame.time.Clock()
 
-    player = load_image('chimp.bmp')
-    background = load_image('fist.bmp')
+    background = load_image('background.bmp')
 
     #draw the background
     screen.blit(background, (0, 0))
@@ -107,117 +109,122 @@ def main():
                     done = True
                     
                 if event.type == KEYDOWN:
+                    if len(letters) == 0:
+                        #clear the whole screen
+                        screen.blit(background, (0, 0))
+                
+                    
                     if event.key == K_0:
                         letters +='0'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_0.png',  letters)
                     elif event.key == K_1:
                         letters +='1'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_1.png',  letters)
                     elif event.key == K_2:
                         letters +='2'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_2.png',  letters)
                     elif event.key == K_3:
                         letters +='3'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_3.png',  letters)
                     elif event.key == K_4:
                         letters +='4'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_4.png',  letters)
                     elif event.key == K_5:
                         letters +='5'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_5.png',  letters)
                     elif event.key == K_6:
                         letters +='6'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_6.png',  letters)
                     elif event.key == K_7:
                         letters +='7'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_7.png',  letters)
                     elif event.key == K_8:
                         letters +='8'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_8.png',  letters)
                     elif event.key == K_9:
                         letters +='9'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0_9.png',  letters)
                     elif event.key == K_a:
                         letters +='a'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0A.png',  letters)
                     elif event.key == K_b:
                         letters +='b'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0B.png',  letters)
                     elif event.key == K_c:
                         letters +='c'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0C.png',  letters)
                     elif event.key == K_d:
                         letters +='d'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0D.png',  letters)
                     elif event.key == K_e:
                         letters +='e'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0E.png',  letters)
                     elif event.key == K_f:
                         letters +='f'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0F.png',  letters)
                     elif event.key == K_g:
                         letters +='g'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0G.png',  letters)
                     elif event.key == K_h:
                         letters +='h'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0H.png',  letters)
                     elif event.key == K_i:
                         letters +='i'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0I.png',  letters)
                     elif event.key == K_j:
                         letters +='j'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0J.png',  letters)
                     elif event.key == K_k:
                         letters +='k'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0K.png',  letters)
                     elif event.key == K_l:
                         letters +='l'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0L.png',  letters)
                     elif event.key == K_m:
                         letters +='m'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0M.png',  letters)
                     elif event.key == K_n:
                         letters +='n'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0N.png',  letters)
                     elif event.key == K_o:
                         letters +='o'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0O.png',  letters)
                     elif event.key == K_p:
                         letters +='p'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0P.png',  letters)
                     elif event.key == K_q:
                         letters +='q'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0Q.png',  letters)
                     elif event.key == K_r:
                         letters +='r'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0R.png',  letters)
                     elif event.key == K_s:
                         letters +='s'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0S.png',  letters)
                     elif event.key == K_t:
                         letters +='t'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0T.png',  letters)
                     elif event.key == K_u:
                         letters +='u'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0U.png',  letters)
                     elif event.key == K_v:
                         letters +='v'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0V.png',  letters)
                     elif event.key == K_w:
                         letters +='w'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0W.png',  letters)
                     elif event.key == K_x:
                         letters +='x'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0X.png',  letters)
                     elif event.key == K_y:
                         letters +='y'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0Y.png',  letters)
                     elif event.key == K_z:
                         letters +='z'
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  '0Z.png',  letters)
                     elif event.key == K_SPACE:
                         letters +=' '
-                        addTile(screen,  bottomTiles,  'tile.bmp',  letters)
+                        addTile(screen,  bottomTiles,  'transparent.png',  letters)
                     elif event.key == K_EXCLAIM:
                         letters +='!'
                         addTile(screen,  bottomTiles,  'tile.bmp',  letters)
@@ -234,10 +241,19 @@ def main():
                         waiting = False
         
         
+        
+       
         #create notes and set their position and speed to raise at
         notes = []
         for x in range(len(letters)):
-            note = ImageObject(player, x*50+10, 500,  3,  letters[x])
+            if ord(letters[x]) >= 97 and ord(letters[x]) <= 122:
+                noteImage = load_image("0" + letters[x].upper() + ".png")
+            elif ord(letters[x]) >= 48 and ord(letters[x]) <= 57:
+                noteImage = load_image("0_" + letters[x] + ".png")
+            elif ord(letters[x]) == 32:
+                      noteImage = load_image("transparent.png")
+      
+            note = ImageObject(noteImage, x*40+10, SCREENHEIGHT - 100,  3,  letters[x])
             notes.append(note)
         
         height = 50
@@ -252,10 +268,10 @@ def main():
 
             accentDelay = 10
             if ord(note.letter) >= 97 and ord(note.letter) <= 122:
-                height = 430 - ((ord(note.letter) - 97) * 15)
+                height = (SCREENHEIGHT-170) - ((ord(note.letter) - 97) * 15)
                 accentDelay = 30 + ((ord(note.letter) - 97) * 5)
             if ord(note.letter) >= 48 and ord(note.letter) <= 57:
-                height = 500 - ((ord(note.letter) - 47 + (122 - 97)) * 20)
+                height = (SCREENHEIGHT-100) - ((ord(note.letter) - 47 + (122 - 97)) * 20)
                 accentDelay = (((ord(note.letter) - 47 + (122 - 97)) * 20) + 20) / 10
             
             #this loops tell the note gets to its destination
